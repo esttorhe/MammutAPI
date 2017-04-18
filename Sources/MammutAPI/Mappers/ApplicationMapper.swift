ac2cae52f4b6670420cec97890e480c4d42085ec
Esteban Torres <me@estebantorr.es>
2017-04-18 09:52:03 +0200

    📁 ApplicationMapper & Tests files Part of #4

---
+++ Sources/MammutAPI/Mappers/ApplicationMapper.swift
@@ -0,0 +1,9 @@
+//
+// Created by Esteban Torres on 18.04.17.
+// Copyright (c) 2017 Esteban Torres. All rights reserved.
+//
+
+import Foundation
+
+internal class ApplicationMapper {
+}
---
+++ Tests/MammutAPITests/Mappers/ApplicationMapperTests.swift
@@ -0,0 +1,11 @@
+//
+// Created by Esteban Torres on 18.04.17.
+// Copyright (c) 2017 Esteban Torres. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import MammutAPI
+
+internal class ApplicationMapperTests: XCTestCase {
+}
