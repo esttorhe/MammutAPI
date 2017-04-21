4ceb5495e38fa9c502eb7eafe85ca997a88da303
Esteban Torres <me@estebantorr.es>
2017-04-21 16:25:13 +0200

    ✨✅ Mention logic and tests

--- Sources/MammutAPI/Models/Mention.swift
+++ Sources/MammutAPI/Models/Mention.swift
@@ -5,5 +5,22 @@
 
 import Foundation
 
-public class Mention {
+public struct Mention {
+    let id: Int
+    let url: URL
+    let username: String
+    let acct: String
+}
+
+// MARK: - Equatable
+
+extension Mention: Equatable {
+    public static func ==(lhs: Mention, rhs: Mention) -> Bool {
+        return (
+                lhs.id == rhs.id &&
+                lhs.url == rhs.url &&
+                lhs.username == rhs.username &&
+                lhs.acct == rhs.acct
+        )
+    }
 }
--- Tests/MammutAPITests/Models/MentionTests.swift
+++ Tests/MammutAPITests/Models/MentionTests.swift
@@ -8,6 +8,17 @@ import XCTest
 @testable import MammutAPI
 
 internal class MentionTests: XCTestCase {
+    func test_sameMentions_equal() {
+        let subject1 = MentionTests.makeMention(id: Int.max)
+        let subject2 = MentionTests.makeMention(id: Int.max)
+        XCTAssertEqual(subject1, subject2)
+    }
+
+    func test_differentMentions_notEqual() {
+        let subject1 = MentionTests.makeMention(id: #line)
+        let subject2 = MentionTests.makeMention(id: #line)
+        XCTAssertNotEqual(subject1, subject2)
+    }
 }
 
 // MARK: - Linux Support
@@ -15,6 +26,21 @@ internal class MentionTests: XCTestCase {
 extension MentionTests {
     static var allTests: [(String, (MentionTests) -> () throws -> Void)] {
         return [
+                ("test_sameMentions_equal", test_sameMentions_equal),
+                ("test_differentMentions_notEqual", test_differentMentions_notEqual)
         ]
+    }
+}
+
+// MARK: Test Helpers
+
+fileprivate extension MentionTests {
+    static func makeMention(id: Int = #line, url: URL = URL(string: "www.example.com")!, username: String = #file, acct: String = #file) -> Mention {
+        return Mention(
+                id: id,
+                url: url,
+                username: username,
+                acct: acct
+        )
     }
 }
