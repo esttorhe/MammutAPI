bd70059c22406673922ea9018a3ec9f68d4dcf3f
Esteban Torres <me@estebantorr.es>
2017-04-21 17:02:08 +0200

    ✨✅💚 Mention mapper logic and tests

--- Sources/MammutAPI/Mappers/MentionMapper.swift
+++ Sources/MammutAPI/Mappers/MentionMapper.swift
@@ -8,8 +8,35 @@ import Foundation
 internal class MentionMapper: ModelMapping {
     typealias Model = Mention
 
-    func map(json: ModelMapping.JSONDictionary) -> Result<Mention, MammutAPIError.MappingError> {
-        return nil
-    }
+    func map(json: ModelMapping.JSONDictionary) -> Result<Model, MammutAPIError.MappingError> {
+        guard
+                let id = json["id"] as? Int,
+                let urlString = json["url"] as? String,
+                let url = URL(string: urlString),
+                let username = json["username"] as? String,
+                let acct = json["acct"] as? String
+                else {
+            return .failure(MammutAPIError.MappingError.incompleteModel)
+        }
 
+        var remoteURL: URL? = nil
+        if let remoteURLString = json["remote_url"] as? String,
+           let url = URL(string: remoteURLString) {
+            remoteURL = url
+        }
+
+        var textURL: URL? = nil
+        if let textURLString = json["text_url"] as? String,
+                let url = URL(string: textURLString) {
+            textURL = url
+        }
+
+        let mention = Mention(
+                id: id,
+                url: url,
+                username: username,
+                acct: acct
+        )
+        return .success(mention)
+    }
 }
--- Tests/MammutAPITests/Mappers/MentionMapperTests.swift
+++ Tests/MammutAPITests/Mappers/MentionMapperTests.swift
@@ -8,7 +8,96 @@ import XCTest
 @testable import MammutAPI
 
 internal class MentionMapperTests: XCTestCase {
-    
+    var subject: MentionMapper!
+
+    override func setUp() {
+        subject = MentionMapper()
+    }
+
+    // MARK: `data` mapping tests
+    func test_map_emptyData_failure_invalidJSON() throws {
+        let result = subject.map(data: Data())
+        let error: MammutAPIError.MappingError = try AssertNotNilAndUnwrap(result.error)
+        XCTAssertEqual(error, MammutAPIError.MappingError.invalidJSON)
+    }
+
+    func test_map_invalidData_failure_incompleteModel() throws {
+        let data = try Fixture.loadData(from: "IncompleteJSON.json")
+        XCTAssertNotNil(data)
+        let result = subject.map(data: data)
+        XCTAssertNil(result.value)
+        let error: MammutAPIError.MappingError = try AssertNotNilAndUnwrap(result.error)
+        XCTAssertEqual(error, MammutAPIError.MappingError.incompleteModel)
+    }
+
+    func test_map_validData_success() throws {
+        let data = try Fixture.loadData(from: "Mention.json")
+        XCTAssertNotNil(data)
+        let result = subject.map(data: data)
+        XCTAssertNil(result.error)
+        XCTAssertNotNil(result.value)
+    }
+
+    func test_map_validData_success_withExpectedValues() throws {
+        let fileName = "Mention.json"
+        let data = try Fixture.loadData(from: fileName)
+        let expectedData = try Fixture.loadJSON(from: fileName)
+        XCTAssertNotNil(expectedData)
+        let result = subject.map(data: data)
+        if case .success(let account) = result {
+            XCTAssertEqual(account.id, expectedData["id"] as! Int)
+            XCTAssertEqual(account.url.absoluteString, expectedData["url"] as! String)
+        } else {
+            XCTFail("Should have returned a parsed «Mention»")
+        }
+    }
+
+    // MARK: `json` mapping tests
+
+    func test_map_invalidJSON_failure_incompleteModel() throws {
+        let json = try Fixture.loadJSON(from: "IncompleteJSON.json")
+        XCTAssertNotNil(json)
+        let result = subject.map(json: json)
+        XCTAssertNil(result.value)
+        let error: MammutAPIError.MappingError = try AssertNotNilAndUnwrap(result.error)
+        XCTAssertEqual(error, MammutAPIError.MappingError.incompleteModel)
+    }
+
+    func test_map_validJSON_success() throws {
+        let json = try Fixture.loadJSON(from: "Mention.json")
+        XCTAssertNotNil(json)
+        let result = subject.map(json: json)
+        XCTAssertNil(result.error)
+        XCTAssertNotNil(result.value)
+    }
+
+    func test_map_validJSON_success_withExpectedValues() throws {
+        let fileName = "Mention.json"
+        let json = try Fixture.loadJSON(from: fileName)
+        let expectedData = try Fixture.loadJSON(from: fileName)
+        XCTAssertNotNil(expectedData)
+        let result = subject.map(json: json)
+        if case .success(let account) = result {
+            XCTAssertEqual(account.id, expectedData["id"] as! Int)
+            XCTAssertEqual(account.url.absoluteString, expectedData["url"] as! String)
+        } else {
+            XCTFail("Should have returned a parsed «Mention»")
+        }
+    }
+
+    func test_mapData_mapJSON_producesSameModel() throws {
+        let fileName = "Mention.json"
+        let json = try Fixture.loadJSON(from: fileName)
+        let data = try Fixture.loadData(from: fileName)
+        XCTAssertNotNil(json)
+        XCTAssertNotNil(data)
+
+        let mappedFromJSON = subject.map(json: json)
+        let mappedFromData = subject.map(data: data)
+        let accountFromJSON: Mention = try AssertNotNilAndUnwrap(mappedFromJSON.value)
+        let accountFromData: Mention = try AssertNotNilAndUnwrap(mappedFromData.value)
+        XCTAssertEqual(accountFromData, accountFromJSON)
+    }
 }
 
 // MARK: - Linux Support
@@ -16,6 +105,14 @@ internal class MentionMapperTests: XCTestCase {
 extension MentionMapperTests {
     static var allTest: [(String, (MentionMapperTests) -> () throws -> Void)] {
         return [
+                ("test_map_emptyData_failure_invalidJSON", test_map_emptyData_failure_invalidJSON),
+                ("test_map_invalidData_failure_incompleteModel", test_map_invalidData_failure_incompleteModel),
+                ("test_map_validData_success", test_map_validData_success),
+                ("test_map_validData_success_withExpectedValues", test_map_validData_success_withExpectedValues),
+                ("test_map_invalidJSON_failure_incompleteModel", test_map_invalidJSON_failure_incompleteModel),
+                ("test_map_validJSON_success", test_map_validJSON_success),
+                ("test_map_validJSON_success_withExpectedValues", test_map_validJSON_success_withExpectedValues),
+                ("test_mapData_mapJSON_producesSameModel", test_mapData_mapJSON_producesSameModel)
         ]
     }
 }
