f60b067db73d5722a9f2ed23c4acd9c2b9ee6848
Esteban Torres <me@estebantorr.es>
2017-04-21 12:15:42 +0200

    📁 Attachment Mapper and tests files

---
+++ Sources/MammutAPI/Mappers/AttachmentMapper.swift
@@ -0,0 +1,9 @@
+//
+// Created by Esteban Torres on 21.04.17.
+// Copyright (c) 2017 Esteban Torres. All rights reserved.
+//
+
+import Foundation
+
+internal class AttachmentMapper {
+}
---
+++ Tests/MammutAPITests/Mappers/AttachmentMapperTests.swift
@@ -0,0 +1,11 @@
+//
+// Created by Esteban Torres on 21.04.17.
+// Copyright (c) 2017 Esteban Torres. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import MammutAPI
+
+internal class AttachmentMapperTests: XCTestCase {
+}
