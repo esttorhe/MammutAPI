c816b5cec340fc67d7befc14856c949885b103fb
Esteban Torres <me@estebantorr.es>
2017-04-23 23:19:29 +0200

    ✨✅ Card mapper and tests

--- Sources/MammutAPI/Mappers/CardMapper.swift
+++ Sources/MammutAPI/Mappers/CardMapper.swift
@@ -5,5 +5,31 @@
 
 import Foundation
 
-internal class CardMapper {
+internal class CardMapper: ModelMapping {
+    typealias Model = Card
+
+    func map(json: ModelMapping.JSONDictionary) -> Result<Card, MammutAPIError.MappingError> {
+        guard
+                let urlString = json["url"] as? String,
+                let url = URL(string: urlString),
+                let title = json["title"] as? String,
+                let description = json["description"] as? String
+                else {
+            return .failure(MammutAPIError.MappingError.incompleteModel)
+        }
+
+        var image: URL? = nil
+        if let imageString = json["image"] as? String,
+           let imageURL = URL(string: imageString) {
+            image = imageURL
+        }
+        
+        let card = Card(
+                url: url,
+                title: title,
+                description: description,
+                image: image
+        )
+        return .success(card)
+    }
 }
